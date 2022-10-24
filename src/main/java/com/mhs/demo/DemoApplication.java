67b1f5d0b49d1a6a0b1dbcca2f5f01a1da0286d5
mhs <cxsdzkcn@gmail.com>
2022-10-24 16:39:20 +0800

    4 commit

--- src/main/java/com/mhs/demo/DemoApplication.java
+++ src/main/java/com/mhs/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+//4提交
 
     public static void main(String[] args) {
 
